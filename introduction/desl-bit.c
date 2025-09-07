f10cddc372a4bb224b7a3264f9a27dbc83bb85ad
Ana Jaqueline <jaquelinesales200729@gmail.com>
2025-09-07 07:19:13 -0300

    Desligar bits
    Melhorias na tarefa de casa pra desligar bits concluída com sucesso!

--- introduction/desl-bit.c
+++ introduction/desl-bit.c
@@ -1,41 +1,30 @@
 /* Exercício (01/09) : A partir deste código, construa uma versão para desligar um 
 bit em uma posição fornecida pelo usuário. */
 #include <stdio.h>
-/*
-int main(){
-    int cod = 219; //     1101 1011
-    int  pos;
-    int mask = 0x01; //   0000 0001
+#include <windows.h>
 
-    // Ligar 
-    puts("Digite a posição que deseja ligar: ");
-    scanf("%d",&pos);
-
-    //Processamento
-    pos = pos - 1;
-    mask = mask << pos;
-    // mask <<= (pos-1);
-    // cod = cod | mask; 
-    cod |= mask;
-
-    printf("Código atualizado: %d\n",cod);
-    
-    return 0;
-}
-*/
 void main(){
+    SetConsoleOutputCP(65001);
+
     int cod = 219; // 1101 1011
     int pos;
     int mask = 0x01;
 
     puts("Digite a posicao que deseja desligar: ");
-    scanf("%hhu",&pos);
+    scanf("%d",&pos);
 
-    pos = pos - 1;
-    printf("Posicao: %hhu \n", pos);
-    mask = mask << pos;
-    printf("Mascara: %hhu \n", mask);
-    cod = cod ^ mask;
-
-    printf("codigo final: %hhu \n",cod);
+    if(pos <= 8){
+        pos = pos - 1;
+        mask = mask << pos;
+        if (cod & mask){
+            cod = cod ^ mask;
+            puts("Condição atualizada com sucesso!");
+        }else{
+            puts("A posição já está desligada. Por gentileza, digite outra posição.");
+        }
+    }else{
+        puts("O número digitado excede o número de posições totais.");
+    }
+    
+    printf("Código final: %hhu \n",cod);
 }
