291f67e14e42f8527636b1cd0c09f55ae53f7cf8
Ana Jaqueline <jaquelinesales200729@gmail.com>
2025-09-21 19:08:40 -0300

    tried 26

---
+++ introduction/lista02/questao26.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+void main(){
+    int binario;
+    int variavel_auxiliar = 0;
+    int variavel_auxiliar2;
+
+    for(int i = 1; i<=256; i++){
+
+        variavel_auxiliar2 = i;
+        for (int j = 8; j >= 0; j--){
+            if((int)pow(2,j) <= variavel_auxiliar2){
+                if(j==0){
+                    variavel_auxiliar+=1;
+                }
+                variavel_auxiliar += (int)pow(10,j);
+                variavel_auxiliar2 -= (int)pow(2,j);
+            }
+        }
+
+        binario = variavel_auxiliar;
+        
+        printf("%d %o %x %d\n",i,i,i, binario);
+    }
+}
