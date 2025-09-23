aa5ff91b0e3c8e074b3874f3ef80685bc5c9f4f4
Jaqueline Sales <jaquelinesales200729@gmail.com>
2025-09-23 15:41:57 -0300

    Update questao26.c

--- introduction/lista02/questao26.c
+++ introduction/lista02/questao26.c
@@ -7,15 +7,19 @@ void main(){
     int variavel_auxiliar2;
 
     for(int i = 1; i<=256; i++){
-
+        variavel_auxiliar = 0;
         variavel_auxiliar2 = i;
         for (int j = 8; j >= 0; j--){
             if((int)pow(2,j) <= variavel_auxiliar2){
                 if(j==0){
                     variavel_auxiliar+=1;
+                } else if(j==0 && i == 1){
+                    variavel_auxiliar = 1;
+                } else {
+                    variavel_auxiliar += (int)pow(10,j);
+                    variavel_auxiliar2 -= (int)pow(2,j);        
                 }
-                variavel_auxiliar += (int)pow(10,j);
-                variavel_auxiliar2 -= (int)pow(2,j);
+
             }
         }
 
